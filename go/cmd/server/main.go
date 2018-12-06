79798dea5709aa12858875d220f43b00cfaa2112
Aren Chen <yarencheng@gmail.com>
2018-12-06 02:29:42 -0800

    test

--- go/cmd/server/main.go
+++ go/cmd/server/main.go
@@ -2,8 +2,12 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+import "github.com/yarencheng/bitcoin-full-node-docker/go/logger"
+
 func main() {
 	r := gin.Default()
+
+	logger.InfoLn("")
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
